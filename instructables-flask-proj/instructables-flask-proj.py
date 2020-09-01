5ddb9ea165bb419db3ebbcdaaefbeda052d29592
collinskoech11 <collinskoechck34@gmail.com>
2020-09-01 03:14:16 +0300

    update

--- instructables-flask-proj/instructables-flask-proj.py
+++ instructables-flask-proj/instructables-flask-proj.py
@@ -2,7 +2,7 @@ from flask import Flask
 from home.views import home_view
 
 def create_app(config_file):
-	app = Flask(__name__)  # Create application object
+	app = Flask(__name__) 
 	app.config.from_pyfile(config_file)  # Configure application with settings file, not strictly necessary
 	app.register_blueprint(home_view)  # Register url's so application knows what to do
 	return app
